ad719629ffedf893d85dbcf8a927c76474cda0e7
frankwang95 <fkwang@uchicago.edu>
2019-09-15 00:40:26 -0700

    Added SQL logging to std dirs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,12 @@
 
 # start MySQL
 mkdir -p /opt/dat/sql
+echo > $LOG_PATH/sql.log
+chmod 777 $LOG_PATH/sql.log
 sed -i "s@datadir.*\$@datadir = /opt/dat/sql@" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s@#port.*\$@port = $SQL_PORT@" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s@#general_log_file.*\$@general_log_file = $LOG_PATH/sql.log@" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s@#general_log.*\$@general_log = 1@" /etc/mysql/mariadb.conf.d/50-server.cnf
 /etc/init.d/mysql start
 
 # persist the running container
