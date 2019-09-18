6d99e3d0969ada7cbf7bfdc08239d664c63e13ad
frankwang95 <fkwang@uchicago.edu>
2019-09-18 01:46:27 -0700

    Pushing non ELK config src.

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,26 @@ FROM baseimage
 ENV SERVICE_NAME shared_services
 ENV BASE_PATH $SVC_PATH/$shared_services
 WORKDIR $BASE_PATH
-RUN mkdir $BASE_PATH/logs
+
 
 # ============= Dependencies ============= #
 RUN apt-get update --fix-missing
-RUN apt-get install --assume-yes default-mysql-server
+RUN apt-get install --assume-yes default-mysql-server curl apt-transport-https gnupg procps gettext
 
+# Filebeat
+RUN mkdir $LIB_PATH/filebeat
+RUN curl -L https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.2-amd64.deb > $LIB_PATH/filebeat/filebeat-7.3.2-amd64.deb
+RUN dpkg -i $LIB_PATH/filebeat/filebeat-7.3.2-amd64.deb
+
+# ElasticSearch, Kibana
+RUN mkdir $LIB_PATH/elastic
+RUN curl -L https://artifacts.elastic.co/GPG-KEY-elasticsearch > $LIB_PATH/elastic/GPG-KEY-elasticsearch
+RUN apt-key add $LIB_PATH/elastic/GPG-KEY-elasticsearch
+RUN echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
+RUN apt-get update
+RUN apt-get install --assume-yes elasticsearch kibana
+
+
+# ============= Source & Entrypoint ============= #
 ADD . $BASE_PATH
 ENTRYPOINT $LIB_PATH/baseimage/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,59 @@
-# load configuration
-. $BASE_PATH/config.sh
+#  ========== load configuration  ========== #
+. $BASE_PATH/ports.sh
 
-# start MySQL
-mkdir -p /opt/dat/sql
-echo > $LOG_PATH/sql.log
-chmod 777 $LOG_PATH/sql.log
-sed -i "s@datadir.*\$@datadir = /opt/dat/sql@" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s@#port.*\$@port = $SQL_PORT@" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s@#general_log_file.*\$@general_log_file = $LOG_PATH/sql.log@" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s@#general_log.*\$@general_log = 1@" /etc/mysql/mariadb.conf.d/50-server.cnf
-/etc/init.d/mysql start
 
-# persist the running container
+#  ========== start MySQL  ========== #
+if [ ! -d $DAT_PATH/sql ]
+then
+    mkdir $DAT_PATH/sql
+    mysql_install_db --datadir=$DAT_PATH/sql --user=mysql
+fi
+/etc/init.d/mysql start \
+    --no-defaults \
+    --port=$SQL_PORT \
+    --character-set-server=utf8mb4 \
+    --collation-server=utf8mb4_general_ci \
+    --datadir=$DAT_PATH/sql \
+    --log-error=$LOG_PATH/error.sql.log \
+    --slow_query_log_file=$LOG_PATH/slow_query.sql.log \
+    --slow_query_log=1 \
+    --long_query_time=2 \
+    --general_log_file=$LOG_PATH/general.sql.log \
+    --general_log=1
+mysql --user=root <<EOF
+    DELETE FROM mysql.user WHERE User='';
+    DELETE FROM mysql.user WHERE User='root' AND Host != 'localhost';
+    DROP DATABASE IF EXISTS test;
+    DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+    FLUSH PRIVILEGES;
+EOF
+
+
+
+# ========== start ELK stack ========== #
+if [ ! -d $DAT_PATH/elk ]
+then
+    elk/initialize_elk.sh
+fi
+
+envsubst < $BASE_PATH/elk/elasticsearch.yml > $BASE_PATH/elk/elasticsearch.yml.tmp
+mv $BASE_PATH/elk/elasticsearch.yml.tmp $BASE_PATH/elk/elasticsearch.yml
+cp $BASE_PATH/elk/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+service elasticsearch start
+
+chmod go-w /opt/svc/elk/filebeat_modules/mysql.yml
+envsubst < $BASE_PATH/elk/filebeat.yml > $BASE_PATH/elk/filebeat.yml.tmp
+mv $BASE_PATH/elk/filebeat.yml.tmp $BASE_PATH/elk/filebeat.yml
+cp $BASE_PATH/elk/filebeat.yml /etc/filebeat/filebeat.yml
+service filebeat start
+
+envsubst < $BASE_PATH/elk/kibana.yml > $BASE_PATH/elk/kibana.yml.tmp
+mv $BASE_PATH/elk/kibana.yml.tmp $BASE_PATH/elk/kibana.yml
+cp $BASE_PATH/elk/kibana.yml /etc/kibana/kibana.yml
+service kibana start
+
+
+ # ========== persist container  ========== #
 while true
 do
     sleep 1
